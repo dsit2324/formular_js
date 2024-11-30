0202f6fb4fc7561940ab99a53b9e35361e42d370
dsit2324 <148313992+dsit2324@users.noreply.github.com>
2024-11-30 17:59:52 +0100

    Update main.js

--- main.js
+++ main.js
@@ -11,9 +11,9 @@ btnCalc.addEventListener('click', function() {
     let path = speed.value * time.value ;
     console.log(path);
     posledniVysledek.push(path);
-    output.innerHTML = `Hello ${firstname.value}, your path value based on speed and time is ${path}.`
+    output.innerHTML = `Hello ${firstname.value}, your path value based on speed and time is ${path} meters.`
     if (posledniVysledek.length > 1){
-        outputArray.innerHTML = `Your previous answer was ${posledniVysledek[posledniVysledek.length - 2]}.`
+        outputArray.innerHTML = `Your previous answer was ${posledniVysledek[posledniVysledek.length - 2]} meters.`
     } else {
         outputArray.innerHTML = `No previous results yet.`;
     }
