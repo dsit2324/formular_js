7392954af24c283ac82d6392529c397f5aa4cec7
dsit2324 <148313992+dsit2324@users.noreply.github.com>
2024-11-30 17:15:50 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+const firstname = document.getElementById('firstname')
+const speed = document.getElementById('speed');
+const time = document.getElementById('time');
+const output = document.getElementById('vystup');
+const outputArray = document.getElementById('vystupPole')
+const btnCalc = document.querySelector('[type=button]');
+
+let posledniVysledek = [];
+
+btnCalc.addEventListener('click', function() {
+    let path = speed.value * time.value ;
+    console.log(path);
+    posledniVysledek.push(path);
+    output.innerHTML = `Hello ${firstname.value}, your path value based on speed and time is ${path}.`
+    if (posledniVysledek.length > 1){
+        outputArray.innerHTML = `Your previous answer was ${posledniVysledek[posledniVysledek.length - 2]}.`
+    } else {
+        outputArray.innerHTML = `No previous results yet.`;
+    }
+});
+
