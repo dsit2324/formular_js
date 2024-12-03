fa75a4325a296a84e4a48011e8df395ad2048949
dsit2324 <148313992+dsit2324@users.noreply.github.com>
2024-12-03 15:00:58 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7,15 +7,32 @@ const btnCalc = document.querySelector('[type=button]');
 
 let posledniVysledek = [];
 
+const calculationData = {
+    name: '',
+    speed: 0,
+    time: 0,
+    path: 0,
+    updateData(name, speed, time, path) {
+        this.name = name;
+        this.speed = speed;
+        this.time = time;
+        this.path = path;
+    }
+};
+
 btnCalc.addEventListener('click', function() {
-    let path = speed.value * time.value ;
+    let path = speed.value * time.value;
     console.log(path);
+
+    calculationData.updateData(firstname.value, speed.value, time.value, path);
+
     posledniVysledek.push(path);
-    output.innerHTML = `Hello ${firstname.value}, your path value based on speed and time is ${path} meters.`
-    if (posledniVysledek.length > 1){
-        outputArray.innerHTML = `Your previous result was ${posledniVysledek[posledniVysledek.length - 2]} meters.`
+
+    output.innerHTML = `Hello ${calculationData.name}, your path value based on speed and time is ${calculationData.path} meters.`;
+
+    if (posledniVysledek.length > 1) {
+        outputArray.innerHTML = `Your previous result was ${posledniVysledek[posledniVysledek.length - 2]} meters.`;
     } else {
         outputArray.innerHTML = `No previous results yet.`;
     }
 });
-
